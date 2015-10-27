31c95774df63cce35aa098a2afc77cc2449f4c2c
Chris Scott <christocracy@gmail.com>
2015-10-27 10:47:54 -0400

    Rename iOS folder

--- ios_delete/AppDelegate.m
+++ ios_delete/AppDelegate.m
@@ -47,6 +47,7 @@
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"RNBackgroundGeolocationSample"
+                                                      initialProperties: nil
                                                    launchOptions:launchOptions];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
