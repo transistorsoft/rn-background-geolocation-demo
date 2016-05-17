c18bff4e8aa9f13359b31ebd26a188b466f795ab
Chris Scott <christocracy@gmail.com>
2016-05-17 01:16:14 +0100

    Remove dev code, jsLocation

--- ios/RNBackgroundGeolocationSample/AppDelegate.m
+++ ios/RNBackgroundGeolocationSample/AppDelegate.m
@@ -31,9 +31,9 @@
    * on the same Wi-Fi network.
    */
 
-  //jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
   //jsCodeLocation = [NSURL URLWithString:@"http://192.168.11.120:8081/index.ios.bundle?platform=ios&dev=true"];
-  jsCodeLocation = [NSURL URLWithString:@"http://172.20.10.4:8081/index.ios.bundle?platform=ios&dev=true"];
+  //jsCodeLocation = [NSURL URLWithString:@"http://172.20.10.4:8081/index.ios.bundle?platform=ios&dev=true"];
   /**
    * OPTION 2
    * Load from pre-bundled file on disk. The static bundle is automatically
