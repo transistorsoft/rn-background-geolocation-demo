d752156200a8213ecfdaea33e6a735be4ef0fd41
Alex Mueller <ajmueller6@gmail.com>
2018-03-02 09:36:52 -0800

    Fixes warning for deprecated icon property for react-native-action-button.

--- src/advanced/HomeView.js
+++ src/advanced/HomeView.js
@@ -616,7 +616,7 @@ export default class HomeView extends Component<{}> {
           active={this.state.isMainMenuOpen}
           backgroundTappable={true}
           onPress={this.onClickMainMenu.bind(this)}
-          icon={<Icon name="ios-add" size={25}/>}
+          renderIcon={() => <Icon name="ios-add" size={25}/>}
           verticalOrientation="down"
           buttonColor="rgba(254,221,30,1)"
           buttonTextStyle={{color: "#000"}}
